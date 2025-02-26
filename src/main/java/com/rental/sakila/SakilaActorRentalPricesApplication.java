5db1e9f2cd030c28eaaee6a4718a89162ccf28f8
dylan <goesdylan@gmail.com>
2025-02-26 09:12:33 +0000

    initial

---
+++ src/main/java/com/rental/sakila/SakilaActorRentalPricesApplication.java
@@ -0,0 +1,13 @@
+package com.rental.sakila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaActorRentalPricesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakilaActorRentalPricesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rental/sakila/SakilaActorRentalPricesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental.sakila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaActorRentalPricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
