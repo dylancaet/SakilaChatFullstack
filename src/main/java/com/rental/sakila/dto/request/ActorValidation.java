deea56fba232fbfd4708587d2f7aaaa7ecc0fcd8
dylan <goesdylan@gmail.com>
2025-03-02 15:48:36 +0000

    the great scrappening

--- src/main/java/com/rental/sakila/dto/request/ActorValidation.java
+++
@@ -1,8 +0,0 @@
-package com.rental.sakila.dto.request;
-
-public interface ActorValidation
-{
-    interface Update {}
-    interface Create {}
-    interface Delete {}
-}
--- src/main/java/com/rental/sakila/repository/StoreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rental.sakila.repository;
-
-import com.rental.sakila.entity.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreRepository extends JpaRepository<Store, Byte> {
-}
