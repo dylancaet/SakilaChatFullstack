0ca1444fa8d2f4e0eea4c966e3931c975fd48111
dylan <goesdylan@gmail.com>
2025-03-04 17:19:35 +0000

    cors annontation

--- src/main/java/com/rental/sakila/controller/StoreController.java
+++ src/main/java/com/rental/sakila/controller/StoreController.java
@@ -5,11 +5,23 @@ import com.rental.sakila.dto.reponse.StoreResponse;
 import com.rental.sakila.entity.Store;
 import com.rental.sakila.service.StoreService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+/*
+    Cross Origin Resource Sharing (CORS)
+    - A security measure implemented by Browsers to restrict cross-origin requests (to stop malicious fetches)
+    - Origins defined in the HTTP header
+    - Browsers will send a `preflight` request before actual request
+    - fetch() follows same-origin policy and can only fetch on the same origin
+        unless the preflight response includes the right CORS headers
+
+    https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+ */
 
 @AllArgsConstructor
 @RestController
@@ -18,6 +30,7 @@ public class StoreController
     private final StoreService service;
 
     /* GET Individual */
+    @CrossOrigin
     @GetMapping(Route.Store.GET_STORE)
     public StoreResponse getStore(@PathVariable Byte id)
     {
@@ -26,6 +39,7 @@ public class StoreController
     }
 
     /* GET List */
+    @CrossOrigin
     @GetMapping(Route.Store.GET_STORE_LIST)
     public List<StoreResponse> getStoreList()
     {
