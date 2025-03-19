4bc095da54838ccad6abdba0f5ec427a99617d0a
dylan <goesdylan@gmail.com>
2025-03-19 14:21:15 +0000

    create film submit action skeleton

--- frontend/src/components/ui/admin/list/CreateFilm.tsx
+++ frontend/src/components/ui/admin/list/CreateFilm.tsx
@@ -1,15 +1,27 @@
 const CreateFilm = () =>
 {
 
+    function handleSubit(formData: FormData)
+    {
+        alert("Submitted");
+    }
+
+
     return (
         <>
-            <form>
-                <label>Title</label>
-                <input type='text' />
-                <label>Description</label>
-                <input type='text' />
-                <label>Price £</label>
-                <input type='number' placeholder='0.00' step='0.5'/>
+            <form action={handleSubit} method='POST'>
+                <label>
+                    Title <input type='text' />
+                </label>
+
+                <label>
+                    Description <input type='text' />
+                </label>
+
+                <label>
+                    Price £ <input type='number' placeholder='0.00' step='0.5'/>
+                </label>
+
                 <button type="submit">Create</button>
             </form>
         </>
